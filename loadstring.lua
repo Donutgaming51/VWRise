5e8d4112554c16252ff89edf0cb156756a2bcee1
Donutgaming51 <jaydenmeade4@gmail.com>
2024-12-18 22:29:07 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,2 +1,2 @@
 shared.RiseMode = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/vapevoidware/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Donutgaming51/straightware/main/NewMainScript.lua", true))()
